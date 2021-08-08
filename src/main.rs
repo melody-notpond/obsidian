7fe03b864518266a7103916191009856a28ae740
jenra-uwu <a>
2021-08-08 17:03:53 -0400

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use std::fs;
+use obsidian::frontend::parser;
+
+fn main() {
+    let contents = fs::read_to_string("example.obs").unwrap();
+    let ast = parser::parse(&contents).unwrap();
+    println!("{:#?}", ast);
+}
